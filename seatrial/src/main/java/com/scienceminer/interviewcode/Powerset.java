e6b0a7a7e0a18dd26ea8bafea144cee6291a6771
scienceMiner <19819315+scienceMiner@users.noreply.github.com>
2020-07-02 23:32:53 +0100

    Add Recursive Powerset call

---
+++ seatrial/src/main/java/com/scienceminer/interviewcode/Powerset.java
@@ -0,0 +1,96 @@
+package com.scienceminer.interviewcode;
+
+import java.util.ArrayList;
+
+public class Powerset {
+
+	public static void  main(String[] args )
+	{
+
+		Powerset ps = new Powerset();
+
+		ArrayList<Integer> inputSetArray = new ArrayList<Integer>();
+		inputSetArray.add(1);
+		inputSetArray.add(2);
+		inputSetArray.add(3);
+		inputSetArray.add(4);
+
+		ArrayList<ArrayList<Integer>> result = ps.powerset(inputSetArray);
+
+		System.out.println( " RESULT: " + result.toString() );
+
+	}
+
+
+	//	Input : [1,2,3]
+	//			add 1 to all powersets of [2,3]	+ all powersets of [2,3] - result [[1,2],[1,2,3],[1],[1,3],  [2],[2,3],[],[3]]
+	//					add 2 to all powersrs of [3] + all powersets of [3]		- result [[2],[2,3],[],[3]]
+	//																			add 3 to all powersets of [] + all powerset of [] - result [[],[3]]
+
+
+	public ArrayList<ArrayList<Integer>> powerset(ArrayList<Integer> inputSet) {
+
+		System.out.println(" POWERSET: " + inputSet.toString() );
+		
+		if (inputSet.size() > 0 ) {
+			
+			Integer first = inputSet.remove(0); // firstItem 
+			
+			ArrayList<ArrayList<Integer>> powerset = powerset(inputSet);
+			System.out.println(" ABOUT to deep Copy powerset:  " + powerset.toString() );
+			
+			ArrayList<ArrayList<Integer>> powerset2 = deepCopy(powerset);
+			System.out.println(" cloned powerset: " + powerset2.toString() );
+			
+			ArrayList<ArrayList<Integer>> addedToAll = addToAll(first,powerset2);
+
+			powerset.addAll(addedToAll);
+		
+			return powerset;
+		}
+		else { // if (inputSet.size() == 0 ) {
+			return new ArrayList<ArrayList<Integer>>();
+		}
+
+	}
+	
+	public ArrayList<ArrayList<Integer>> deepCopy( ArrayList<ArrayList<Integer>> inputArray ) {
+		
+		ArrayList<ArrayList<Integer>> inputClone = new ArrayList<ArrayList<Integer>>();
+
+		for (ArrayList<Integer> singleArray : inputArray )
+		{
+			ArrayList<Integer> newSingleArray = new ArrayList<Integer> ();
+			
+			newSingleArray.addAll(singleArray);
+			inputClone.add(newSingleArray);
+		}
+	
+		return inputClone;
+	}
+
+	private Integer[][] append(Integer[][] addedToAll, Integer[][] powerset) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public ArrayList<ArrayList<Integer>> addToAll(Integer first,  ArrayList<ArrayList<Integer>> tails  ) {
+
+		System.out.println( ">>>>>>>  addToAll: " + first + " to " + tails.toString() );
+
+		for (ArrayList<Integer> inArr : tails ) {
+			inArr.add(first);
+		}
+
+		// Add the single Integer as its own entry - FATAIL initial mistake was to leave this out - OUCH!!
+		ArrayList<Integer> intArray = new ArrayList<Integer>();
+		intArray.add(first);
+		tails.add(intArray);
+
+		return tails;
+
+	}
+	
+
+}
