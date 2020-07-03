3b579a8065350d3fec38d5595260612864c51130
scienceMiner <19819315+scienceMiner@users.noreply.github.com>
2020-07-03 16:48:32 +0100

    Update to log4j2

--- seatrial/src/main/java/com/scienceminer/interviewcode/Powerset.java
+++ seatrial/src/main/java/com/scienceminer/interviewcode/Powerset.java
@@ -2,11 +2,12 @@ package com.scienceminer.interviewcode;
 
 import java.util.ArrayList;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Powerset {
 
-	final static Logger logger = Logger.getLogger(Powerset.class);
+	final static Logger logger = LogManager.getLogger("Powerset");
 
 	public static void  main(String[] args )
 	{
